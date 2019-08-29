a1af3bf5c74bf687770141cfea23bd3b4ada6fc1
Piotr Serafin <piotr_serafin@epam.com>
2019-08-29 13:28:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# c++ dev environment based on docker and vscode remote extension
+
+## Prerequisites
+
+* docker
+* vscode-insiders
+* ms-vscode-remote.remote-containers
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+
+    return 0;
+}
